234c1e3cfa5b545b816a776d0c61753634725383
PredaaA <46051820+PredaaA@users.noreply.github.com>
2022-01-01 00:25:29 +0100

    Update license.

--- nekosbest/client.py
+++ nekosbest/client.py
@@ -1,6 +1,6 @@
 """
 A simple async Python wrapper for nekos.best API.
-Copyright (C) 2021  PredaaA
+Copyright (C) 2021-present  PredaaA
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as published
