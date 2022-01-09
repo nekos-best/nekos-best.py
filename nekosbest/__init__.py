1599c21db6aebb4ffebfa4a6e867ca1db017887e
PredaaA <46051820+PredaaA@users.noreply.github.com>
2022-01-09 20:49:16 +0100

    Release 1.1.3

--- nekosbest/__init__.py
+++ nekosbest/__init__.py
@@ -16,7 +16,7 @@ You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
-__version__ = "1.1.2"
+__version__ = "1.1.3"
 __author__ = "PredaaA"
 __copyright__ = "Copyright 2021 PredaaA"
 
