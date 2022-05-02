abbad632946f766f993edb3391bfb12677d8a4be
PredaaA <46051820+PredaaA@users.noreply.github.com>
2022-05-03 00:24:15 +0200

    1.7.3 - Add new endpoints support.

--- nekosbest/__init__.py
+++ nekosbest/__init__.py
@@ -16,7 +16,7 @@ You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
-__version__ = "1.1.6"
+__version__ = "1.1.7"
 __author__ = "PredaaA"
 __copyright__ = "Copyright 2021-present PredaaA"
 
--- nekosbest/models.py
+++ nekosbest/models.py
@@ -56,6 +56,10 @@ CATEGORIES = (
     "wink",
     "kitsune",
     "waifu",
+    "handhold",
+    "kick",
+    "punch",
+    "shoot",
 )
 
 
